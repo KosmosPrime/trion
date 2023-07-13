b5557eb23956ce2f4cc1d7e11681141ba2aeece5
KosmosPrime <5663514+KosmosPrime@users.noreply.github.com>
2023-07-13 16:53:28 +0200

    Add a generic position-annotated type

--- src/text/mod.rs
+++ src/text/mod.rs
@@ -1,2 +1,29 @@
+use core::fmt;
+use std::error::Error;
+
 pub mod parse;
 pub mod token;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Positioned<T>
+{
+	pub value: T,
+	pub line: u32,
+	pub col: u32,
+}
+
+impl<T: fmt::Display> fmt::Display for Positioned<T>
+{
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result
+	{
+		write!(f, "{} ({}:{})", self.value, self.line, self.col)
+	}
+}
+
+impl<T: Error> Error for Positioned<T>
+{
+	fn source(&self) -> Option<&(dyn Error + 'static)>
+	{
+		self.value.source()
+	}
+}
