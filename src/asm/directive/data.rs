4bdfab84e2b065101d438ec537706d63a1588cd8
KosmosPrime <5663514+KosmosPrime@users.noreply.github.com>
2023-08-01 17:58:30 +0200

    Implement .dhex as a Directive

--- src/asm/directive/data.rs
+++ src/asm/directive/data.rs
@@ -9,7 +9,7 @@ use crate::text::token::Number;
 
 macro_rules!generate
 {
-	($name:ident($type:ty) as $label:literal => |$ctx:ident, $active:ident, $value:ident| {$write:expr}) =>
+	($name:ident($($argt:tt)+) as $label:literal => |$ctx:ident, $active:ident, $args:ident, $value:ident| {$($write:tt)+}) =>
 	{
 		#[derive(Clone, Copy, Debug)]
 		pub struct $name;
@@ -21,57 +21,112 @@ macro_rules!generate
 				$label
 			}
 			
-			fn apply<'c>(&self, $ctx: &'c mut Context, args: Positioned<&[Argument]>) -> Result<(), &'c DirectiveError>
+			fn apply<'c>(&self, $ctx: &'c mut Context, $args: Positioned<&[Argument]>) -> Result<(), &'c DirectiveError>
 			{
 				let Some($active) = $ctx.active_mut()
 				else
 				{
-					return Err($ctx.push_error(args.convert(DirectiveErrorKind::Apply(Box::new(DataError::Inactive)))));
+					return Err($ctx.push_error($args.convert(DirectiveErrorKind::Apply(Box::new(DataError::Inactive)))));
 				};
-				if args.value.len() != 1
+				if $args.value.len() != 1
 				{
-					return Err($ctx.push_error(args.convert_fn(|v| DirectiveErrorKind::ArgumentCount{min: Some(1), max: Some(1), have: v.len()})))
+					return Err($ctx.push_error($args.convert_fn(|v| DirectiveErrorKind::ArgumentCount{min: Some(1), max: Some(1), have: v.len()})))
 				}
-				let $value = match args.value[0]
+				let $value = generate!(@impl $($argt)+ = $args[0], $ctx, $active);
+				if let Err(e) = {$($write)+}
 				{
-					Argument::Constant(ref num) =>
-					{
-						let &Number::Integer(val) = num;
-						match <$type>::try_from(val)
-						{
-							Ok(val) => val,
-							_ =>
-							{
-								let err = Box::new(DataError::Range{min: i64::from(<$type>::MIN), max: i64::from(<$type>::MAX), have: val});
-								return Err($ctx.push_error(args.convert(DirectiveErrorKind::Apply(err))));
-							},
-						}
-					},
-					ref arg =>
-					{
-						let err = DirectiveErrorKind::Argument{idx: 0, expect: ArgumentType::Constant, have: arg.get_type()};
-						return Err($ctx.push_error(args.convert(err)));
-					},
-				};
-				if let Err(e) = $write
-				{
-					return Err($ctx.push_error(args.convert(DirectiveErrorKind::Apply(Box::new(DataError::Write(e))))));
+					return Err($ctx.push_error($args.convert(DirectiveErrorKind::Apply(Box::new(DataError::Write(e))))));
 				}
 				Ok(())
 			}
 		}
-	}
+	};
+	(@impl Constant($type:ty) = $args:ident[$idx:expr], $ctx:ident, $active:ident) =>
+	{
+		match $args.value[$idx]
+		{
+			Argument::Constant(ref num) =>
+			{
+				let &Number::Integer(val) = num;
+				match <$type>::try_from(val)
+				{
+					Ok(val) => val,
+					_ =>
+					{
+						let err = Box::new(DataError::Range{min: i64::from(<$type>::MIN), max: i64::from(<$type>::MAX), have: val});
+						return Err($ctx.push_error($args.convert(DirectiveErrorKind::Apply(err))));
+					},
+				}
+			},
+			ref arg =>
+			{
+				let err = DirectiveErrorKind::Argument{idx: 0, expect: ArgumentType::Constant, have: arg.get_type()};
+				return Err($ctx.push_error($args.convert(err)));
+			},
+		}
+	};
+	(@impl String = $args:ident[$idx:expr], $ctx:ident, $active:ident) =>
+	{
+		match $args.value[$idx]
+		{
+			Argument::String(ref string) => string.as_ref(),
+			ref arg =>
+			{
+				let err = DirectiveErrorKind::Argument{idx: $idx, expect: ArgumentType::String, have: arg.get_type()};
+				return Err($ctx.push_error($args.convert(err)));
+			},
+		}
+	};
 }
 
-generate!(DataU8(u8) as "du8" => |ctx, active, value| {active.write(core::slice::from_ref(&(value as u8)))});
-generate!(DataU16(u16) as "du16" => |ctx, active, value| {active.write(&u16::to_le_bytes(value))});
-generate!(DataU32(u32) as "du32" => |ctx, active, value| {active.write(&u32::to_le_bytes(value))});
+generate!(DataU8(Constant(u8)) as "du8" => |ctx, active, args, value| {active.write(core::slice::from_ref(&(value as u8)))});
+generate!(DataU16(Constant(u16)) as "du16" => |ctx, active, args, value| {active.write(&u16::to_le_bytes(value))});
+generate!(DataU32(Constant(u32)) as "du32" => |ctx, active, args, value| {active.write(&u32::to_le_bytes(value))});
+generate!
+{
+	DataHex(String) as "dhex" => |ctx, active, args, in_hex|
+	{
+		let mut out_data = Vec::new();
+		let mut carry: Option<u8> = None;
+		for (pos, c) in in_hex.chars().enumerate().filter(|(_, c)| !c.is_ascii_whitespace())
+		{
+			match c.to_digit(16)
+			{
+				None =>
+				{
+					let err = Box::new(DataError::HexChar{pos, value: c});
+					return Err(ctx.push_error(args.convert(DirectiveErrorKind::Apply(err))));
+				},
+				Some(v) =>
+				{
+					let v = v as u8;
+					match carry
+					{
+						None => carry = Some(v << 4),
+						Some(c) =>
+						{
+							out_data.push(v | c);
+							carry = None;
+						},
+					}
+				},
+			}
+		}
+		if carry.is_some()
+		{
+			return Err(ctx.push_error(args.convert(DirectiveErrorKind::Apply(Box::new(DataError::HexEof)))));
+		}
+		active.write(out_data.as_slice())
+	}
+}
 
 #[derive(Debug)]
 pub enum DataError
 {
 	Inactive,
 	Range{min: i64, max: i64, have: i64},
+	HexChar{pos: usize, value: char},
+	HexEof,
 	Write(WriteError),
 }
 
@@ -83,6 +138,8 @@ impl fmt::Display for DataError
 		{
 			Self::Inactive => f.write_str("no active segment to write to"),
 			Self::Range{min, max, have} => write!(f, "constant out of range ({min} to {max}, got {have})"),
+			Self::HexChar{pos, value} => write!(f, "invalid hex char {value:?} at {pos}"),
+			Self::HexEof => f.write_str("unexpected eof in hex string"),
 			Self::Write(..) => f.write_str("could not write alignment bytes"),
 		}
 	}
